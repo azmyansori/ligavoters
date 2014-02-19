63ae65072ce0cadb168d8c094e765f896c3d8c62
Gilang Satria Prayoga <gilang@dirac.local>
2014-02-19 21:44:01 +0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ligavoters::Application.config.secret_key_base = 'd1a9f32cdb9d7c4e7d42dd747c22523febac35b1eebcffec8cbd0861beec6babad1613442b528c960da6a374ad356173266e26c919f1b080e89255c605d403d1'
